d1c2ec89fc3ac96568bb7531c6199d1086756822
JSANN <zenwink@gmail.com>
2022-07-05 16:59:00 +0800

    feat: dist

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,20 @@
 import typescript from '@rollup/plugin-typescript';
 import replace from '@rollup/plugin-replace';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
 import less from 'rollup-plugin-less';
 import pkg from './package.json';
 
 const input = 'src/img-crop.tsx';
 const exports = 'auto';
-const deps = [...Object.keys(pkg.dependencies), ...Object.keys(pkg.peerDependencies)];
+const deps = [
+  // ...Object.keys(pkg.dependencies),
+  ...Object.keys(pkg.peerDependencies),
+];
 const external = (id) => deps.includes(id) || id.startsWith('antd');
 const plugins = (isESM) => [
+  commonjs(),
+  resolve(),
   typescript(),
   !isESM && replace({ preventAssignment: true, '/es/': '/lib/' }),
   less({ insert: true, output: false }),
